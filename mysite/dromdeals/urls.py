e955a853813e9c7041116af7aa02f82ffa5c31a1
swapnil <swapnillawande2001@gmail.com>
2025-03-31 23:01:05 +0200

    v6

--- mysite/dromdeals/urls.py
+++ mysite/dromdeals/urls.py
@@ -8,7 +8,13 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name="about_page"),
     path('post/<int:pk>/', views.ProductDetailView.as_view(), name='post_detail'),
     path('post/new/', views.CreateProductView.as_view(), name='post_new'),
-    path('post/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='post_edit')
+    path('post/<int:pk>/edit/', views.ProductUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/remove/', views.ProductDeleteView.as_view(), name='post_remove'),
+    path('post/<int:pk>/comments/', views.add_comments_to_product_post, name='add_comments_to_post'),
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+
 
 
 ]
--- mysite/dromdeals/views.py
+++ mysite/dromdeals/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from dromdeals import models
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.decorators import login_required
 from dromdeals import forms
 from django.urls import reverse_lazy
+from django.shortcuts import redirect, get_object_or_404, render
 
 
 # Create your views here.
@@ -51,3 +53,53 @@ class ProductUpdateView(UpdateView):
 class ProductDeleteView(LoginRequiredMixin, DeleteView):
     model = models.PostProduct
     success_url = reverse_lazy('post_list')
+
+
+# Draft list view
+class DraftListView(LoginRequiredMixin, ListView):
+    login_url = '/login/'
+    redirect_field_name = 'dromdeals/post_list.html'
+
+
+
+############################### PRODUCT COMMENTS #######################################
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(models.PostProduct, pk=pk)
+    post.publish
+    return redirect('post_detail' ,pk=pk)
+
+
+@login_required
+def add_comments_to_product_post(request, pk):
+
+    post = get_object_or_404(models.PostProduct, pk=pk)
+
+    if request.method =="POST":
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+
+    return (request, 'dromdeals/comment_from.html', {'form': form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk = comment.post.pk)
+
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
